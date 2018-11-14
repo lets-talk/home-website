15f1e778098e80f2e63fd68596d4ab3a7f8ce962
cristianferrarig <cristianferrarig@gmail.com>
2018-11-14 15:44:40 -0300

    Improve config file

--- config.rb
+++ config.rb
@@ -1,18 +1,12 @@
 #
-# BASEKIT CONFIG
+# WEBSITE CONFIG
 # Version 0.1
 #
 # 1. Directories
 # 2. Assets
 # 3. Layouts
-# 4. Helpers
-# 5. Extensions
-# 6. Options
-# 7. Eviroments
-# 8. Deploy
+# 4. Eviroments
 #
-
-
 
 # 1. DIRECTORIES
 
@@ -24,6 +18,7 @@ set :locales_dir,    "locales"
 set :helpers_dir,    "helpers"
 
 # Relative to source
+
 set :layouts_dir,    "layouts"
 set :partials_dir,   "partials"
 
@@ -31,8 +26,6 @@ set :css_dir,        "stylesheets"
 set :js_dir,         "javascripts"
 set :images_dir,     "images"
 set :fonts_dir,      "fonts"
-
-
 
 # 2. ASSETS
 
@@ -43,92 +36,21 @@ after_configuration do
     sprockets.append_path File.join( root, "styles/" )
 end
 
-# sprockets.append_path "#{root}/styles/"
-
-# after_configuration do
-#   sprockets.append_path "#{root}/styles"
-# sprockets.append_path "#{root}/assets/stylesheets"
-# sprockets.append_path "#{root}/assets/javascripts"
-# sprockets.append_path "#{root}/vendor/bower"
-# sprockets.import_asset 'jquery'
-# end
-
-
-
-# 3. LAYOUTS
-
-# https://middlemanapp.com/basics/layouts/
-
-# set :layout,         "default"
-# page '/*', layout: "default"
-
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-
-
-# 4. HELPERS
-
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-
-
-# 5. EXTENSIONS
-
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# activate :syntax do |syntax|
-#   syntax.css_class     = "code-highlight"
-#   syntax.line_numbers  = true
-# end
+# 3. LAYOUTS
 
-# activate :syntax, :line_numbers => true
-# activate :syntax, line_numbers: true, css_class: "code-highlight"
+activate :directory_indexes
 
-# activate :i18n, :data => "locales"
-# activate :directory_indexes
+set :index_file, "home.html"
 
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-
-# 6. OPTIONS
-
-# set :relative_links, false
-# set :show_exceptions, true
-
-
-
-# 7. ENVIROMENTS
-
-# Build-specific after_configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+# 4. ENVIROMENTS
 
 configure :development do
   activate :livereload
@@ -136,27 +58,8 @@ configure :development do
 end
 
 configure :build do
-  # activate :imageoptim
   activate :minify_css
   activate :minify_javascript
   activate :cache_buster
-  # activate :asset_hash
-  # activate :relative_assets
-  # set :relative_links, false
 end
 
-
-
-# 8. DEPLOY
-
-# activate :deploy do |deploy|
-#     deploy.deploy_method = :git
-#     deploy.build_before = true
-# end
-
-# Optional Settings
-
-# deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-# deploy.branch   = 'custom-branch' # default: gh-pages
-# deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-# deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
